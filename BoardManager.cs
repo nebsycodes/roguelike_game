3f80e04675557e39e6cc61a091da49bfac787434
James <145176861+nebsycodes@users.noreply.github.com>
2025-02-08 10:00:40 +1100

    0.2

--- BoardManager.cs
+++ BoardManager.cs
@@ -3,28 +3,50 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
-    public class CellData
+   public class CellData
+   {
+      public bool Passable;
+   }
+
+   private CellData[,] m_BoardData;
+   private Tilemap m_Tilemap;
+   public int Width;
+   public int Height;
+   public Tile[] GroundTiles;
+   public Tile[] WallTiles;
+}
+
+// Start is called before the first frame update
+void Start()
+{
+    m_Tilemap = GetComponentInChildren<Tilemap>();
+
+
+    for (int y = 0; y < Height; ++y)
     {
-        public bool Passable;
+        for(int x = 0; x < Width; ++x)
+        {
+            Tile tile;
+              
+            if(x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
+            {
+                tile = WallTiles[Random.Range(0, WallTiles.Length)];
+            }
+            else
+            {
+                tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+            }
+              
+            m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+        }
     }
+}
 
-    private CellData[,] m_BoardData;
-    private Tilemap m_Tilemap;
-    private Grid m_Grid;  // Declared m_Grid
 
-    public int Width;
-    public int Height;
-    public Tile[] GroundTiles;
-    public Tile[] WallTiles;
+public PlayerController Player;
 
-    public PlayerController Player;
-
-    public Vector3 CellToWorld(Vector2Int cellIndex)
-    {
-        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
-    }
-
-    void Start()
+// Start is called before the first frame update
+void Start()
 {
    m_Tilemap = GetComponentInChildren<Tilemap>();
    m_Grid = GetComponentInChildren<Grid>();
